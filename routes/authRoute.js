a559c1abe10a44daf3eb8d0ffe2e0aa335e4cfcb
Sumit Bhimte <bhimtesumit118@gmail.com>
2023-06-19 17:13:08 +0530

    Initial commit

---
+++ routes/authRoute.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { registerController, loginController, testController } from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+
+//router object
+const router = express.Router();
+
+//routing
+//REGISTER || METHOD POST
+router.post("/register", registerController);
+
+// LOGIN || POST
+router.post("/login", loginController);
+
+//test routes
+// requireSignIn isAdmin are middlewares
+router.get("/test", requireSignIn, isAdmin, testController);
+
+//protected route auth
+router.get("/user-auth", requireSignIn, (req, res) => {
+    res.status(200).send({ ok: true });
+});
+
+export default router;
