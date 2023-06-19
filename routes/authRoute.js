e41b3659e022874099adf0be6e9ae607d15e71de
Sumit Bhimte <bhimtesumit118@gmail.com>
2023-06-19 19:49:26 +0530

    updated forgot pass function

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerController, loginController, testController } from "../controllers/authController.js";
+import { registerController, loginController, testController, forgotPasswordController } from "../controllers/authController.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
 //router object
@@ -12,6 +12,9 @@ router.post("/register", registerController);
 // LOGIN || POST
 router.post("/login", loginController);
 
+//Forgot Password || POST
+router.post("/forgot-password", forgotPasswordController);
+
 //test routes
 // requireSignIn isAdmin are middlewares
 router.get("/test", requireSignIn, isAdmin, testController);
@@ -21,4 +24,8 @@ router.get("/user-auth", requireSignIn, (req, res) => {
     res.status(200).send({ ok: true });
 });
 
+//protected Admin route auth
+router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
+    res.status(200).send({ ok: true });
+});
 export default router;
